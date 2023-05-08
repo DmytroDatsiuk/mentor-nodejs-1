dcc0e5677e93339adde82ddfa7a82c46cf2b5f94
DDimbe <109931951+DmytroDatsiuk@users.noreply.github.com>
2023-05-08 06:53:40 +0300

    lesson

---
+++ routes/tasksRouter.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const {
+  getTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask,
+} = require("../controller/tasksControllers");
+
+const router = express.Router();
+
+router.get("/", getTasks);
+
+router.get("/:id", getTaskById);
+
+router.post("/", createTask);
+
+router.patch("/:id", updateTask);
+
+router.delete("/:id", deleteTask);
+
+module.exports = { tasksRouter: router };
